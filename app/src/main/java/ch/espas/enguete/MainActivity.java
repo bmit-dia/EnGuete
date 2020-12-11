8b9f7066399c0add4e7563f155f4132ad47b15e2
Alessandro <alessandro.dimaria@espas.ch>
2020-12-11 10:40:48 +0100

    Adding Notification Service with real time checker.

--- app/src/main/java/ch/espas/enguete/MainActivity.java
+++ app/src/main/java/ch/espas/enguete/MainActivity.java
@@ -1,6 +1,11 @@
 package ch.espas.enguete;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,16 +15,19 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Calendar;
+
 import ch.espas.enguete.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        imageView = (ImageView) findViewById(R.id.imageView4);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -32,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //Notification
+        Intent notifyIntent = new Intent(this, NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast
+                (this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+
+        Calendar targetTime = Calendar.getInstance();
+        targetTime.setTimeInMillis(System.currentTimeMillis());
+        targetTime.set(Calendar.HOUR_OF_DAY, 12);
+        targetTime.set(Calendar.MINUTE, 00);
+        targetTime.set(Calendar.SECOND, 00);
+
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
+                1000 * 60 * 60 * 24, pendingIntent);
+
     }
 
 }
---
+++ app/src/main/java/ch/espas/enguete/NotificationReceiver.java
@@ -0,0 +1,17 @@
+package ch.espas.enguete;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    public NotificationReceiver(){
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Intent intent1 = new Intent(context, MyNewIntentService.class);
+        context.startService(intent1);
+    }
+}
